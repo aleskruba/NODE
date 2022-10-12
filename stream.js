bf2e2292ae5c6241ae510f251409ee71156554da
aleskruba <74367962+aleskruba@users.noreply.github.com>
2022-10-12 11:06:46 +0200

    Create stream.js

---
+++ stream.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+const rs = fs.createReadStream('lorem.txt', {encoding:'utf8'});
+
+const ws = fs.createWriteStream('new-lorem.txt');
+
+/*
+rs.on('data',(dataChunk)=>{
+  ws.write(dataChunk)
+})
+
+*/
+
+rs.pipe(ws)
